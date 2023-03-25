aec26b1d30ff1270ca2f4bc27d547613f49b671d
rajbalaraj <rajbalasr15@gmail.com>
2023-03-26 00:10:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const products = [
+  //   { name: 'laptopoo', price: 153000 },
+  //   { name: 'phoneee', price: 93000 },
+  //   { name: 'watchuu', price: 33000 },
+  //   { name: 'tabletoo', price: 5000 },
+  // ]
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Counter></Counter>
+      {/* {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      } */}
+      {/* <Product name="laptop" price=" 50000"></Product> */}
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(55)
+  // const increaseCopunt = () => {
+  //   const newCount = count + 1;
+  //   setacount(newCount);
+  // }
+
+  // shortcut
+  const increaseCopunt = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1)
+
+
+
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCopunt}>Increase</button>
+      <button onClick={decreaseCount}>Decerease</button>
+    </div>
+  )
+}
+
+function Product(props) {
+
+  return (
+    <div>
+      {/* <h3>Name:{props.name}</h3> */}
+      <p>Price:{props.price}</p>
+    </div>
+  )
 }
 
 export default App;
