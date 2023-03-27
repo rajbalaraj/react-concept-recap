1377efd805cb34cb7901e40a6d1e785b87700ba6
rajbalaraj <rajbalasr15@gmail.com>
2023-03-27 17:34:01 +0600

    fetch-add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   // const products = [
@@ -13,7 +13,9 @@ function App() {
 
     <div className="App">
 
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUser></ExternalUser>
+      {/* <User></User> */}
       {/* {
         products.map(product => <Product name={product.name} price={product.price}></Product>)
       } */}
@@ -22,6 +24,33 @@ function App() {
   );
 }
 
+function ExternalUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+  return (
+    <div>
+      <h2>external user</h2>
+      {
+        users.map(user => <User name={user.name}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div style={{
+      border: '2px solid red', margin: '2px'
+    }}>
+      <h3>name:{props.name}</h3>
+
+    </div>
+  )
+}
 function Counter() {
   const [count, setCount] = useState(55)
   // const increaseCopunt = () => {
